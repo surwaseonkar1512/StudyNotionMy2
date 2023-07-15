6559d1c5843b97b28b3a658a676f30a3736249dd
onkar surwase <surwaseonkar12345@gmail.com>
2023-07-15 22:30:54 +0530

    first commit

---
+++ src/data/navbar-links.js
@@ -0,0 +1,19 @@
+export const NavbarLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+ 
+  {
+    title: "About Us",
+    path: "/about",
+  },
+  {
+    title: "Contact Us",
+    path: "/contact",
+  },
+  {
+    title: "Catalog",
+    // path: '/catalog',
+  },
+];
